092fc0be35d826c7e43ec5dd6049c6dfd75c040a
Igor Smirnov <uchetzapis@yahoo.com>
2023-02-16 17:15:53 +0300

    At least compiled Product module

--- src/first/product.ts
+++ src/first/product.ts
@@ -15,84 +15,111 @@ export class Product {
 export function filterProducts(products: Product[], queries: string): Product[] {
     const conditions = queries
         .split('&')
-        .map((query) => Condition.create<Product, keyof Product>(query));
+        .map((query) => Condition.create(query));
 
     console.log(conditions);
 
-    products.filter((product) => {
+    return products.filter((product) => {
         return conditions.every((condition) => condition.check(product));
-    })
-    return products;
+    });
 }
 
 type Comparison<T> = (t1: T, t2: T) => boolean
 
-type NumberComparison = '<' | '=' | '>' | '<=' | '>=';
-type string = 'contains' | 'starts' | 'ends';
+// type NumberComparison = '<' | '=' | '>' | '<=' | '>=';
+// type string = 'contains' | 'starts' | 'ends';
 
-class Condition<P, Key extends keyof P> {
-    private readonly property: Key;
-    private readonly comparison: Comparison<P[Key]>;
-    private readonly value: P[Key];
+class Condition<V extends string | number> {
+    private readonly property: string;
+    private readonly comparison: Comparison<V>;
+    private readonly value: V;
 
-    private constructor(property: Key, comparison: Comparison<P[Key]>, value: P[Key]) {
+    private constructor(property: string, comparison: Comparison<V>, value: V) {
         this.property = property;
         this.comparison = comparison;
         this.value = value;
     }
 
-    check(product: P): boolean {
-        const productValue = product[this.property];
-        return this.comparison(this.value, productValue)
+    check(product: Product): boolean {
+        const property = this.property;
+        if (property in product) {
+            const productValue: Product[keyof Product] = (product as any)[property] as Product[keyof Product];
+            return this.comparison(this.value, productValue as V)
+        }
+        return false;
     }
 
-    public static create<Type, K extends keyof Type>(query: string): Condition<Type, K> {
-        const [prop, ...how] = query.split('-')
-        if (prop !== undefined && how.length === 2) { // string comparison
+    public static create(query: string): Condition<number> | Condition<string> {
+        let [property, ...how] = query.split('-')
+        property = property ?? ''
+
+        if (how.length === 2) { // string comparison
             const [comparisonSymbol, value] = how;
-            new Condition(prop, Condition.stringComparison(comparisonSymbol), value)
+   
+           new Condition<string>(property, Condition.stringComparison(comparisonSymbol ?? ''), value ?? '')
 
         } else if (how.length === 1) {  // number comparison
+            const { comparison, value } = Condition.comp(how[0] ?? '')
         
-            new Condition(prop, Comparison.numberComparison('>='), 10)
+            new Condition<number>(property, comparison, value)
 
         }
-        return new Condition(prop, () => true, "")
+        // incorrect string
+        return new Condition<any>('', () => false, '')
     }
 
 
-    private readonly static stringComparison(comparison: string): (s1: string, s2: string) => boolean {
+    private static stringComparison(comparison: string): Comparison<string> {
         return function(value: string, substring: string) {
             switch(comparison) {
                 case 'contains':
-                    return value.includes(substring) ?? false;
+                    return value.includes(substring);
                 case 'starts':
                     return value.startsWith(substring)
                 case 'ends':
                     return value.endsWith(substring);
-                default:
+                default: // incorrect operation
                     return false;
             }
         }
     }
 
-    private readonly static numberComparison(comparisonSymbol: string): (x: number. y: number) => boolean {
-        return function(value: number, referencedValue: number) {
-            switch (comparisonSymbol) {
-                case '<':
-                    return value < referencedValue;
-                case '>':
-                    return value > referencedValue;
-                case '=':
-                    return value === referencedValue;
-                case '<=':
-                    return value <= referencedValue;
-                case '>=':
-                    return value >= referencedValue;
-                default:
-                    return false;
+    private static comp(how: string): { comparison: Comparison<number>, value: number } {
+        if (how.startsWith('>=')) {
+            return {
+                value: +how.slice(2),
+                comparison: (x, y) => x >= y,
             }
         }
-    }
+        if (how.startsWith('<=')) {
+            return {
+                value: +how.slice(2),
+                comparison: (x, y) => x <= y,
+            }
+        }
+        if (how.startsWith('>')) {
+            return {
+                value: +how.slice(1),
+                comparison: (x, y) => x > y,
+            }
+        }
+        if (how.startsWith('<')) {
+            return {
+                value: +how.slice(1),
+                comparison: (x, y) => x < y,
+            }
+        }
+        if (how.startsWith('=')) {
+            return {
+                value: +how.slice(1),
+                comparison: (x, y) => x === y,
+            }
+        }
 
+        return {  // incorrect operation
+            value: NaN,
+            comparison: () => false,
+        }
+        
+    }
 }
